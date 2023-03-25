8faeb2ecdc5901f692562d2f49763c60308b493e
Kory Kliner <kory.kliner@outlook.com>
2023-03-25 09:50:28 -0400

    clean up directories

--- build/build.ps1
+++ build/build.ps1
@@ -1,7 +1,8 @@
 $baseXmlPath = '.\source\Scheduled-TaskBase.xml'
 $scriptPath = '.\source\Get-Data.ps1'
-$releaseXmlPath = './intune/Scheduled-Task.xml'
+$releaseXmlPath = './release/intune-source/Scheduled-Task.xml'
 
+# update xml used for scheduled task
 [xml]$baseXml = Get-Content -Path $baseXmlPath
 $script = Get-Content -Path $scriptPath -Raw
 $baseXml.Task.Actions.Exec.Arguments += '-NoProfile -WindowStyle Hidden -Command "& {'
@@ -10,3 +11,6 @@ $baseXml.Task.Actions.Exec.Arguments += $script
 $baseXml.Task.Actions.Exec.Arguments += '}"'
 $baseXml.Task.Actions.Exec.Arguments += "`n      "
 $baseXml.Save($releaseXmlPath)
+
+# build intunewin
+.\source\IntuneWinAppUtil.exe -c ".\release\intune-source" -s "install.ps1" -o ".\release\w32-app" -q
